befe4a14963850a851402ab676b12ae6b26894ce
MariaE003 <elgotbymaria@gmail.com>
2025-12-28 19:48:44 +0100

    les challenges 3

--- Sql-challenges.sql
+++ Sql-challenges.sql
@@ -13,3 +13,28 @@ GROUP BY s.coach_id
 
 Challenge 2:
 5 -
+/*ch2 - le nombre des reservation par moi*/
+SELECT u.nom,u.prenom, month(se.date_seance) as mois,date_format(se.date_seance,"%Y") as date,COUNT(*) as nbr from users u 
+INNER JOIN sportifs s on s.user_id=u.id 
+INNER JOIN reservations r on r.sportif_id=s.user_id 
+INNER JOIN seances se on se.id=r.seance_id 
+GROUP by mois,u.id ,date
+ORDER BY nbr DESC;
+
+Challenge 3:
+/**/
+
+SELECT * FROM seances se1
+join seances se2 on se1.coach_id=se2.coach_id
+and se2.id<se1.id
+and se1.heure<date_add(se2.heure,interval se2.duree minute)
+and se2.heure<date_add(se1.heure,interval se1.duree minute)
+
+/**/
+SELECT se1.coach_id,se1.date_seance,se1.heure,date_add(se1.heure,interval se1.duree minute),se1.id FROM seances se1
+join seances se2 on se1.coach_id=se2.coach_id
+and se2.id<se1.id
+and se1.heure<date_add(se2.heure,interval se2.duree minute)
+and se2.heure<date_add(se1.heure,interval se1.duree minute)
+
+Challenge 4:
